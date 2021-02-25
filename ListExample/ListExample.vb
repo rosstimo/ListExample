810006de34bcc5f6b87b2b04116026fd6406c15a
rosstimo <rosstimo@isu.edu>
2021-02-25 08:45:22 -0700

    Add project files.

---
+++ ListExample/ListExample.vb
@@ -0,0 +1,33 @@
+﻿Option Compare Text
+Option Strict On
+Option Explicit On
+Module ListExample
+
+    Sub Main()
+
+        Dim names As New List(Of String)
+
+        names.Add("Tim")
+        names.Add("Joe")
+        names.Add("Mary")
+        names.Add("Joe")
+
+        'names.Sort()
+        'Console.WriteLine(names.IndexOf("Joe"))
+        'Console.WriteLine(names.Item(2))
+
+        names.Add("Frank")  'adds to end
+        names.Insert(2, "Bob") 'insert Bob into index 2
+
+        names.Remove("Joe") 'remove first occurrence of Joe 
+        'names.RemoveAt(3) 'remove whatever is at index 3
+
+        For Each name In names
+            Console.WriteLine($"{name} is at index {names.IndexOf(name)}")
+        Next
+
+        Console.WriteLine(names.Count)
+        Console.Read()
+    End Sub
+
+End Module
